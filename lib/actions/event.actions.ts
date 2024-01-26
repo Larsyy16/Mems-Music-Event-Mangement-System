4863795803577df0043ddd8cebfd3ecd4e7c25f8
Lars <lars.kolankiewicz@gmail.com>
2024-01-25 23:29:12 -0500

    add getEventByID to event.actions

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -6,7 +6,7 @@ import Event from "@/lib/database/models/event.model";
 import User from "@/lib/database/models/user.model";
 import { handleError } from "@/lib/utils";
 import { CreateEventParams } from "@/types";
-import { Tag } from "lucide-react";
+import Tag from "../database/models/tag.model";
 
 export async function createEvent({ userId, event, path }: CreateEventParams) {
   try {
@@ -28,3 +28,30 @@ export async function createEvent({ userId, event, path }: CreateEventParams) {
     handleError(error);
   }
 }
+
+
+const populateEvent = (query: any) => {
+  return query
+    .populate({ path: 'organizer', model: User, select: '_id firstName lastName' })
+    .populate({ path: 'category', model: Tag, select: '_id name' })
+}
+
+// GET ONE EVENT BY ID
+export async function getEventById(eventId: string) {
+  try {
+    await connectToDatabase()
+
+    const event = await populateEvent(Event.findById(eventId))
+
+    if (!event) throw new Error('Event not found')
+
+    return JSON.parse(JSON.stringify(event))
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+
+//i have to get imageURL
+//fix no price error
+
