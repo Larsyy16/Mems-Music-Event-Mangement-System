9d1344f3ae57eea6cde714d10ac6bf5c93b00db7
Lars <lars.kolankiewicz@gmail.com>
2024-01-27 19:31:25 -0500

    config update & file deletion

--- components/shared/FileUploader.tsx
+++ components/shared/FileUploader.tsx
@@ -1,16 +1,14 @@
-'use client'
+// "use client";
 
-import { Dispatch, SetStateAction, useState } from "react"
+// import { Dispatch, SetStateAction, useState } from "react";
 
-type FileUploadProps = {
-    imageUrl: string
-    onFieldChange: (value: string) => void
-    setFiles: Dispatch<SetStateAction<File[]>>
-}
+// type FileUploadProps = {
+//   imageUrl: string;
+//   onFieldChange: (value: string) => void;
+//   setFiles: Dispatch<SetStateAction<File[]>>;
+// };
 
-
-export default function FileUploader() {
-    const [file, setFile] = useState<File | null>(null)
-    const [uploading, setUploading] = useState(false)
-
-  }
+// export default function FileUploader() {
+//   const [file, setFile] = useState<File | null>(null);
+//   const [uploading, setUploading] = useState(false);
+// }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['memsbucket2024.s3.us-east-2.amazonaws.com'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'memsbucket2024.s3.us-east-2.amazonaws.com',
+                port: ''
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
