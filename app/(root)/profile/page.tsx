b5b6c4bc3cb0b1841b92731b6fac71a6372ae8e3
Lars <lars.kolankiewicz@gmail.com>
2024-03-11 23:39:32 -0400

    add h1 to profile page

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -27,9 +27,9 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
       {/* My Tickets */}
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <div className="wrapper flex items-center justify-center sm:justify-between">
-          <h3 className="h3-bold text-center sm:text-left text-white">
+          <h1 className="h3-bold text-center sm:text-left text-white">
             My Tickets
-          </h3>
+          </h1>
           <Button asChild size="lg" className="button hidden sm:flex">
             <Link href="/#events">Explore More Events</Link>
           </Button>
